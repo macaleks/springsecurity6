7c01bc49b80cd029be20ca2502cf79b291e912ed
Aleksander <msxander@gmail.com>
2024-08-08 08:32:31 +0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# springsecurity6
+GET http://localhost:8080/oauth2/authorize?response_type=code&client_id=client-id&redirect_uri=http://localhost:8080/login/oauth2/code/client-id&scope=read
+
+
+
+
+
+
+curl -X POST \
+http://localhost:8090/oauth2/token \
+-H "Content-Type: application/x-www-form-urlencoded" \
+-d "grant_type=password&username=user&password=password&client_id=my-client-id&client_secret=my-client-secret"
+
+
+
+curl -H "Authorization: Bearer <your-access-token>" http://localhost:8081/api/hello
+
+
+
+
+
+curl -X POST \
+http://localhost:8090/oauth2/token \
+-H "Content-Type: application/x-www-form-urlencoded" \
+-d "grant_type=password&username=user&password=password&client_id=client-id&client_secret=client-secret&scope=read"
+
+
+
+
+Error 403 No valid crumb was included in the request
+.
+
+
+curl -X GET http://localhost:8090/oauth2/authorize?response_type=code&client_id=client-id&user=user&password=password
+
+
+http://localhost:8080/login/oauth2/code/client-id?code=SplxlOBeZQQYbYS6WxSbIA
+
+
+http://localhost:8080/login/oauth2/code/client-id?code=SplxlOBeZQQYbYS6WxSbIA
+
+
+curl -X POST \
+http://localhost:8080/oauth2/token \
+-H "Content-Type: application/x-www-form-urlencoded" \
+-d "grant_type=authorization_code&code=SplxlOBeZQQYbYS6WxSbIA&redirect_uri=http://localhost:8080/login/oauth2/code/client-id&client_id=client-id&client_secret=client-secret"
---
+++ main/java/org/example/demo/AuthorizationServerConfig.java
@@ -0,0 +1,75 @@
+package org.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.UUID;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class AuthorizationServerConfig {
+
+    @Bean
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+        http
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/oauth2/token", "/oauth2/authorize"))
+                .httpBasic(withDefaults())
+                .formLogin(withDefaults())
+        ;
+
+//        http.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest()
+//                .authenticated());
+//                .csrf(csrf -> csrf.disable());
+//                .cors(cors -> cors.disable());
+        return http.build();
+    }
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository(PasswordEncoder passwordEncoder) {
+        RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("client-id")
+                .clientSecret(passwordEncoder.encode("client-secret"))
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .authorizationGrantType(AuthorizationGrantType.PASSWORD)
+                .redirectUri("http://localhost:8080/login/oauth2/code/client-id")
+                .scope("read")
+                .scope("write")
+                .build();
+
+        return new InMemoryRegisteredClientRepository(registeredClient);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
